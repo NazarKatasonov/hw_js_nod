d280b6240523cedd099a1b30a1c9049f7aa27a9c
NazarKatasonov <nazariikryt22@gmail.com>
2026-01-28 23:35:06 +0500

    fwgfbwegfy

--- src/js/hw02.js
+++ src/js/hw02.js
@@ -1,34 +1,74 @@
+const galleryList = document.querySelector('.js-gallery');
+const lightbox = document.querySelector('.js-lightbox');
+const lightboxImage = document.querySelector('.lightbox__image');
+const closeButton = document.querySelector(
+  'button[data-action="close-lightbox"]',
+);
+const overlay = document.querySelector('.lightbox__overlay');
 
-  const slider = document.querySelector('.slider__input');
-  const image = document.querySelector('.slider__image');
 
-  function debounce(fn, delay) {
-    let timeoutId;
-    return function (...args) {
-      clearTimeout(timeoutId);
-      timeoutId = setTimeout(() => {
-        fn.apply(this, args);
-      }, delay);
-    };
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      item => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+          />
+        </a>
+      </li>
+    `,
+    )
+    .join('');
+}
+
+
+galleryList.innerHTML = createGalleryMarkup(galleryItems);
+
+
+galleryList.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
   }
 
-  const resizeImage = debounce((value) => {
-    image.style.width = value + '%';
-  }, 200);
+  const largeImageURL = event.target.dataset.source;
+  const imageAlt = event.target.alt;
 
-  slider.addEventListener('input', (e) => {
-    resizeImage(e.target.value);
-  });
+  openLightbox(largeImageURL, imageAlt);
+}
 
 
+function openLightbox(src, alt) {
+  lightbox.classList.add('is-open');
+  lightboxImage.src = src;
+  lightboxImage.alt = alt;
+
+  window.addEventListener('keydown', onEscKeyPress);
+}
 
 
-  const box = document.getElementById('box');
+function closeLightbox() {
+  lightbox.classList.remove('is-open');
+  lightboxImage.src = '';
+  lightboxImage.alt = '';
 
-  const moveBox = _.debounce((event) => {
-    box.style.left = event.clientX + 'px';
-    box.style.top = event.clientY + 'px';
-  }, 100);
+  window.removeEventListener('keydown', onEscKeyPress);
+}
 
-  document.addEventListener('mousemove', moveBox);
+closeButton.addEventListener('click', closeLightbox);
+overlay.addEventListener('click', closeLightbox);
 
+
+function onEscKeyPress(event) {
+  if (event.code === 'Escape') {
+    closeLightbox();
+  }
+}
